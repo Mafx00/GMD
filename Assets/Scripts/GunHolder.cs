d4a04f5f1ff016a0a15b8683670b816657a807fc
Mafx00 <43205840+Mafx00@users.noreply.github.com>
2020-04-03 15:07:06 +0200

    pick up guns and health

---
+++ Assets/Scripts/GunHolder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunHolder : MonoBehaviour
+{
+    public GameObject gun;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(pickGunUp());
+
+    }
+
+    public IEnumerator pickGunUp()
+    {
+        gun.SetActive(false);
+        yield return new WaitForSeconds(5);
+        gun.SetActive(true);
+    }
+}
